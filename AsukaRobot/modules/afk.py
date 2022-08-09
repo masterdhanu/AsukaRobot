2b93b51feabe7264f34f8ef97a4ae8fd7505dddc
ARAN アラン <109020344+AranXSiesta@users.noreply.github.com>
2022-08-09 13:42:11 +0530

    Update afk.py

--- AsukaRobot/modules/afk.py
+++ AsukaRobot/modules/afk.py
@@ -20,7 +20,7 @@ from AsukaRobot.modules.helper_funcs.readable_time import get_readable_time
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
 
-AFKVID = "https://telegra.ph/file/c49829f9f5d65947cbc7e.mp4"
+AFKVID = "https://te.legra.ph/file/d39ec5ce492c2e37cb045.mp4"
 
 
 def afk(update, context):
@@ -38,7 +38,7 @@ def afk(update, context):
     fname = update.effective_user.first_name
     try:
         update.effective_message.reply_video(
-            AFKVID,caption="Byii Byiii {}!".format(fname))
+            AFKVID,caption="Bye {}!".format(fname))
     except BadRequest:
         pass
 
